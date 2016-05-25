89ebe7e3166c4a2ed3ca2e4d95088823852febfc
Tautvydas Milciunas <Tautvydas Milciunas>
2016-05-25 14:02:53 +0300

    Added class "Traukinys" with it's methods

--- 2016_3_s1413155.py
+++ 2016_3_s1413155.py
@@ -7,10 +7,8 @@ class Lokomotyvas:
         self.maxMase = maxMase
     def __add__(self,other):
         return(self.mase+other.mase,self.maxMase+other.maxMase)
-    
     def __sub__(self,other):
         return(self.mase-other.mase,self.maxMase-other.maxMase)
-
     def __repr__(self):
         return "(Mase: %d,%d)"%(self.mase,self.maxMase)
     def __str__(self):
@@ -25,12 +23,27 @@ class Vagonas:
     def __str__(self):
         return "Mase: %i, Max mase: %i, Krovinio mase: %i, Nr: %i" %(self.mase,self.maxMase,self.krovinioMase,self.nr)
 
+class Traukinys:
+    def __init__(self,lok,vag):
+        self.lok = lok
+        self.vag = vag
+    def __repr__(self):
+        return "Lok: %s Vag: %s" % (self.lok,self.vag)
+    def __str__(self):
+        return "Lokomatyvas: %d Vagonas: %d" % (self.lok,self.vag)
+    def __add__(self,other):
+        return Traukinys(self.lok.mase+other.lok.mase,self.vag.mase+other.vag.mase)
+    def __sub__(self,other):
+        return Traukinys(self.lok.mase-other.lok.mase,self.vag.mas-+other.vag.mase)
+    def __len__(self):
+		return len(Traukinys)
+    def __bool__(self):
+		return False
+            
 Lokomotyvas1 = Lokomotyvas(1,2)
 Lokomotyvas2 = Lokomotyvas(13,3)
 Vag = Vagonas(1,2,3,4)
 
-lokas = Lokomotyvas(0,0)
+Traukinys1 = Traukinys(Lokomatyvas1,Vag)
 
-lokas = Lokomotyvas1 + Lokomotyvas2
-print(lokas)
-print("git test")
+print(Traukinys1)
