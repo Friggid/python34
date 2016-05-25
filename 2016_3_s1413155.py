183b220b64f8902f24b1d7d989469773c30bf8d7
Tautvydas Milciunas <Tautvydas Milciunas>
2016-05-25 16:35:40 +0300

    Testing free space calculation

--- 2016_3_s1413155.py
+++ 2016_3_s1413155.py
@@ -17,6 +17,8 @@ class Vagonas:
         self.maxMase = maxMase
         self.krovinioMase = krovinioMase
         self.nr = nr
+    def __add__(self,other):
+        return(self.mase+other.mase,self.maxMase+other.maxMase)
     def __str__(self):
         return "Mase: %i, Max mase: %i, Krovinio mase: %i, Nr: %i" %(self.mase,self.maxMase,self.krovinioMase,self.nr)
 
@@ -27,21 +29,65 @@ class Traukinys:
     def __repr__(self):
         return "Lok: %s Vag: %s" % (self.lok,self.vag)
     def __str__(self):
-        return "Lokomotyvo mase: %d,Vagono mase: %d" %(self.lok.mase,self.vag.mase)
+        return "Lokomotyvo mase: %d,Vagono mase: %d" %(self.lok,self.vag)
     def __add__(self,other):
-        return Traukinys(self.lok.mase+other.lok.mase,self.vag.mase+other.vag.mase)
+        trauk_mase = self.lok.mase+other.lok.mase
+        vag_mase = self.vag.mase+other.vag.mase
+        mase = trauk_mase + vag_mase
+        
+        return mase
+        #return Traukinys(self.lok.mase+other.lok.mase,self.vag.mase+other.vag.mase)
     def __sub__(self,other):
-        return Traukinys(self.lok.mase-other.lok.mase,self.vag.mas-+other.vag.mase)
+        maxMaseVagono = self.vag.maxMase+other.vag.maxMase
+        maxMaseLokoloko = self.lok.maxMase+other.lok.maxMase
+        maxMase = maxMaseVagono + maxMaseLokoloko
+        
+        uzimtaVagonu = self.vag.krovinioMase + other.vag.krovinioMase 
+        
+        uzimtaMase = maxMase - uzimtaVagonu
+        
+        return uzimtaMase
     def __len__(self):
 	    return len(Traukinys)
     def __bool__(self):
 	    return False
             
-Lokomotyvas1 = Lokomotyvas(1,2)
-Lokomotyvas2 = Lokomotyvas(13,3)
-Vag = Vagonas(1,2,3,4)
-Vag2 = Vagonas(5,6,7,8)
+Lok1 = Lokomotyvas(1,10)
+Lok2 = Lokomotyvas(13,20)
+Lok3 = Lokomotyvas(2,5)
+Lok4 = Lokomotyvas(20,30)
+Vag1 = Vagonas(5,10,3,1)
+Vag2 = Vagonas(9,20,7,2)
+Vag3 = Vagonas(9,20,10,3)
+Vag4 = Vagonas(8,20,9,4)
 
-Traukinys1 = Traukinys(Lokomotyvas2,Vag)
+Trauk1 = Traukinys(Lok1,Vag1)
+Trauk2 = Traukinys(Lok2,Vag2)
+Trauk3 = Traukinys(Lok3,Vag3)
+Trauk4 = Traukinys(Lok4,Vag4)
 
-print(Traukinys1)
+Laisva1 = (Trauk1-Trauk2) - (Trauk1+Trauk2)
+Laisva2 = (Trauk3-Trauk4) - (Trauk3+Trauk4)
+print(Laisva1)
+print(Laisva2)
+
+
+# Sastatas = Vag+Vag2
+
+# Traukinys1 = Traukinys(Lokomotyvas2,Vag)
+# Traukinys2 = Traukinys(Lokomotyvas1,Vag2)
+
+# TraukMase = Traukinys1+Traukinys2
+# TraukUzimta = Traukinys1-Traukinys2 
+# laisva = TraukMase - TraukUzimta
+#14,6
+# add(14+6,kroviniomase)
+#print(TraukMase)
+#print(TraukUzimta)
+#print(laisva)
+
+#{"vagonas": [{"nr": 1, "mase": 20, "maxMase": 40, "krovinioMase": 10}], "lokomotyvas": [{"maxMase": 3, "mase": 2}, {"maxMase": 1, "mase": 1}]}
+
+# lok.mase+vag.mase+vag.krovinioMase = Traukinio mase
+# vag.maxMase+lok.maxMase = Max traukinio mase
+# Max traukinio mase - traukinio mase = laisva vieta
