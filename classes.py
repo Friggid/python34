c7604c649032f5375c9411e2cc8d453177407a28
Tautvydas Milciunas <Tautvydas Milciunas>
2016-05-25 18:16:16 +0300

    sorting and json reading

--- classes.py
+++ classes.py
@@ -19,6 +19,8 @@ class Vagonas:
         self.nr = nr
     def __add__(self,other):
         return(self.mase+other.mase,self.maxMase+other.maxMase)
+    def __repr__(self):
+        return "(Mase: %d, Max mase: %d, Krovinio mase: %d, Nr: %d)" %(self.mase,self.maxMase,self.krovinioMase,self.nr)    
     def __str__(self):
         return "Mase: %i, Max mase: %i, Krovinio mase: %i, Nr: %i" %(self.mase,self.maxMase,self.krovinioMase,self.nr)
 
@@ -27,13 +29,13 @@ class Traukinys:
         self.lok = lok
         self.vag = vag
     def __repr__(self):
-        return "Lok: %s Vag: %s" % (self.lok,self.vag)
+        return "(Lok: %s Vag: %s)" % (self.lok,self.vag)
     def __str__(self):
         return "Lokomotyvo mase: %d,Vagono mase: %d" %(self.lok,self.vag)
     def __add__(self,other):
-        trauk_mase = self.lok.mase+other.lok.mase
+        lok_mase = self.lok.mase+other.lok.mase
         vag_mase = self.vag.mase+other.vag.mase
-        mase = trauk_mase + vag_mase
+        mase = lok_mase + vag_mase
         
         return mase
         #return Traukinys(self.lok.mase+other.lok.mase,self.vag.mase+other.vag.mase)
@@ -42,52 +44,13 @@ class Traukinys:
         maxMaseLokoloko = self.lok.maxMase+other.lok.maxMase
         maxMase = maxMaseVagono + maxMaseLokoloko
         
-        uzimtaVagonu = self.vag.krovinioMase + other.vag.krovinioMase 
+        krovinioMase = self.vag.krovinioMase + other.vag.krovinioMase 
         
-        uzimtaMase = maxMase - uzimtaVagonu
+        uzimtaMase = maxMase - krovinioMase
         
         return uzimtaMase
     def __len__(self):
 	    return len(Traukinys)
     def __bool__(self):
 	    return False
-            
-Lok1 = Lokomotyvas(1,10)
-Lok2 = Lokomotyvas(13,20)
-Lok3 = Lokomotyvas(2,5)
-Lok4 = Lokomotyvas(20,30)
-Vag1 = Vagonas(5,10,3,1)
-Vag2 = Vagonas(9,20,7,2)
-Vag3 = Vagonas(9,20,10,3)
-Vag4 = Vagonas(8,20,9,4)
 
-Trauk1 = Traukinys(Lok1,Vag1)
-Trauk2 = Traukinys(Lok2,Vag2)
-Trauk3 = Traukinys(Lok3,Vag3)
-Trauk4 = Traukinys(Lok4,Vag4)
-
-Laisva1 = (Trauk1-Trauk2) - (Trauk1+Trauk2)
-Laisva2 = (Trauk3-Trauk4) - (Trauk3+Trauk4)
-print(Laisva1)
-print(Laisva2)
-
-
-# Sastatas = Vag+Vag2
-
-# Traukinys1 = Traukinys(Lokomotyvas2,Vag)
-# Traukinys2 = Traukinys(Lokomotyvas1,Vag2)
-
-# TraukMase = Traukinys1+Traukinys2
-# TraukUzimta = Traukinys1-Traukinys2 
-# laisva = TraukMase - TraukUzimta
-#14,6
-# add(14+6,kroviniomase)
-#print(TraukMase)
-#print(TraukUzimta)
-#print(laisva)
-
-#{"vagonas": [{"nr": 1, "mase": 20, "maxMase": 40, "krovinioMase": 10}], "lokomotyvas": [{"maxMase": 3, "mase": 2}, {"maxMase": 1, "mase": 1}]}
-
-# lok.mase+vag.mase+vag.krovinioMase = Traukinio mase
-# vag.maxMase+lok.maxMase = Max traukinio mase
-# Max traukinio mase - traukinio mase = laisva vieta
--- readwrite.py
+++ readwrite.py
@@ -1,12 +1,91 @@
 import json
 import sys
+import classes
+from classes import * 
 
-def readWriteJson():
+def readJson():
     #failas = input()
-              # '/mif/stud3/2014/tami1864/Python/3atsisk/'+ cia failas turi but
     with open("data.json") as data_file:
         data=json.load(data_file)
+    
+    lok = []
+    vag = []
+    trauk = []
+    for i in range(len(data["lokomotyvas"])):
+        lok.append(Lokomotyvas(data['lokomotyvas'][i]['mase'],data['lokomotyvas'][i]['maxMase']))
+    
+    for i in range(len(data["vagonas"])):
+        vag.append(Vagonas(data['vagonas'][i]['nr'],data['vagonas'][i]['mase'],data['vagonas'][i]['maxMase'],data['vagonas'][i]['krovinioMase']))
+    
+    # print(lok[0])
+    # print(vag[0])
+    trauk.append(Traukinys(lok[0],vag[0]))
+    trauk.append(Traukinys(lok[1],vag[1]))
+    trauk.append(Traukinys(lok[2],vag[2]))
+    return trauk
+    
+def sorting():      
+    trauk = []
+    trauk = readJson()
+    print(trauk)
+    
+    Mase = []
+    Mase.append((trauk[0]+trauk[1]))
+    Laisva = []
+    Laisva.append((trauk[0]+trauk[1]) - (trauk[0]-trauk[1]))
+    Laisva.append((trauk[1]+trauk[2]) - (trauk[1]-trauk[2]))
+    Laisva.append((trauk[2]+trauk[0]) - (trauk[2]-trauk[0]))
+    
+    Laisva = sorted(Laisva,reverse=False)
+    
+    print(Laisva)
+    print(Mase)
+    # Lok1 = Lokomotyvas(1,10)
+    # Lok2 = Lokomotyvas(13,20)
+    # Lok3 = Lokomotyvas(2,5)
+    # Lok4 = Lokomotyvas(20,30)
 
+    # Vag1 = Vagonas(5,10,3,1)
+    # Vag2 = Vagonas(9,20,7,2)
+    # Vag3 = Vagonas(9,20,10,3)
+    # Vag4 = Vagonas(8,20,9,4)
+
+    # Trauk1 = Traukinys(Lok1,Vag1)
+    # Trauk2 = Traukinys(Lok2,Vag2)
+    # Trauk3 = Traukinys(Lok3,Vag3)
+    # Trauk4 = Traukinys(Lok4,Vag4)
+
+    
+
+    # Laisva = sorted(Laisva,reverse=False)
+    # print(Laisva)
+########################################################    
+sorting()
+    # Sastatas = Vag+Vag2
+
+    # Traukinys1 = Traukinys(Lokomotyvas2,Vag)
+    # Traukinys2 = Traukinys(Lokomotyvas1,Vag2)
+
+    # TraukMase = Traukinys1+Traukinys2
+    # TraukUzimta = Traukinys1-Traukinys2 
+    # laisva = TraukMase - TraukUzimta
+    #14,6
+    # add(14+6,kroviniomase)
+    #print(TraukMase)
+    #print(TraukUzimta)
+    #print(laisva)
+
+    #{"vagonas": [{"nr": 1, "mase": 20, "maxMase": 40, "krovinioMase": 10}], "lokomotyvas": [{"maxMase": 3, "mase": 2}, {"maxMase": 1, "mase": 1}]}
+
+    # lok.mase+vag.mase+vag.krovinioMase = Traukinio mase
+    # vag.maxMase+lok.maxMase = Max traukinio mase
+    # Max traukinio mase - traukinio mase = laisva vieta
+
+def writeJson():
+#failas = input()
+             ## '/mif/stud3/2014/tami1864/Python/3atsisk/'+ cia failas turi but
+    with open("data.json") as data_file:
+        data=json.load(data_file)
 
     tipas = input("1. Lokomotyvas, 2. Vagonas. Parasykite pasirinkima: ")
 
@@ -41,4 +120,3 @@ def readWriteJson():
     else:
         print("Neteisingas pasirinkimas")
         sys.exit(0)
-readWriteJson()
